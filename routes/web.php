592f783a3381e73fe82907b13b6f94eaf2cc620e
0xMeowchii <soldevillamauchilan@gmail.com>
2026-03-31 15:25:54 +0800

    Eloquent and Model Factorties

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,19 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use App\Models\Job;
+use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/jobs', function (){
+Route::get('/jobs', function () {
     return view('jobs', [
         'jobs' => Job::all(),
     ]);
 });
 
-Route::get('/jobs/{id}', function ($id){
+Route::get('/jobs/{id}', function ($id) {
     $job = Job::find($id);
 
     return view('job', ['job' => $job]);
