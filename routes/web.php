e909db0c55da957453c2a81aca3cf27167092c0d
0xMeowchii <soldevillamauchilan@gmail.com>
2026-04-03 17:22:17 +0800

    Forms, CSRF, CRUD, and Validation

--- routes/web.php
+++ routes/web.php
@@ -7,18 +7,74 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+// index
 Route::get('/jobs', function () {
-    $jobs = Job::with('employer')->paginate(5);
+    $jobs = Job::with('employer')->latest()->paginate(5);
 
-    return view('jobs', [
-        'jobs' => $jobs
+    return view('jobs.index', [
+        'jobs' => $jobs,
     ]);
 });
 
+// Create
+Route::get('/jobs/create', function () {
+    return view('jobs.create');
+});
+
+// Show
 Route::get('/jobs/{id}', function ($id) {
     $job = Job::find($id);
 
-    return view('job', ['job' => $job]);
+    return view('jobs.show', ['job' => $job]);
+});
+
+// Store
+Route::post('/jobs', function () {
+    request()->validate([
+        'title' => ['required', 'min:3'],
+        'salary' => ['required', 'numeric', 'min_digits:4'],
+    ]);
+
+    Job::create([
+        'title' => request('title'),
+        'salary' => request('salary'),
+        'employer_id' => 1,
+    ]);
+
+    return redirect('/jobs');
+});
+
+// edit
+Route::get('/jobs/{id}/edit', function ($id) {
+    $job = Job::find($id);
+
+    return view('jobs.edit', ['job' => $job]);
+});
+
+// update
+Route::patch('/jobs/{id}', function ($id) {
+    request()->validate([
+        'title' => ['required', 'min:3'],
+        'salary' => ['required', 'numeric', 'min_digits:4'],
+    ]);
+
+    $job = Job::findOrfail($id);
+
+    $job->update([
+        'title' => request('title'),
+        'salary' => request('salary')
+    ]);
+
+    return redirect('/jobs/' . $job->id);
+});
+
+// destroy
+Route::delete('/jobs/{id}', function ($id) {
+    $job = Job::findorfail($id);
+
+    $job->delete();
+
+    return redirect('/jobs');
 });
 
 Route::get('/contact', function () {
